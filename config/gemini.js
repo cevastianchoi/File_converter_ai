2bc79d4cb31de53a5997a5f5b8a2935d6f5dacc8
Byung Ouk Choi <seb.dev.it@gmail.com>
2025-07-08 12:41:12 +0900

    exp file size optimizing ver 1.0

--- config/gemini.js
+++ config/gemini.js
@@ -1,5 +1,5 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: './config.env' });
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
